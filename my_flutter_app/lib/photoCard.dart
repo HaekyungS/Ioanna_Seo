a733c84e9ae5d6e99d61c0c71fd2bce7b1fabf12
HaekyungSeong <haekyungs95@gmail.com>
2023-10-12 15:20:10 +0900

    파일명 수정 및 상단에 앱으로 정정 하고 위젯을 추가해서 넣는 방식으로 하기

--- my_flutter_app/lib/photoCard.dart
+++ my_flutter_app/lib/photoCard.dart
@@ -5,18 +5,13 @@ class PhotoCardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Row(children: [
-        ElevatedButton(onPressed: () {}, child: const Text('2017')),
-        ElevatedButton(onPressed: () {}, child: const Text('2018')),
-        ElevatedButton(onPressed: () {}, child: const Text('2019')),
-        ElevatedButton(onPressed: () {}, child: const Text('2020')),
-        ElevatedButton(onPressed: () {}, child: const Text('2021')),
-        ElevatedButton(onPressed: () {}, child: const Text('2022')),
-        ElevatedButton(onPressed: () {}, child: const Text('2023'))
-      ]),
-      const Text('공사즁 뚜따뚜따')
-    ]);
+    return MaterialApp(
+        theme: ThemeData(useMaterial3: true),
+        home: const Scaffold(
+          body: Row(
+            children: [Text('안농'), Text('안농?')],
+          ),
+        ));
   }
 }
 
