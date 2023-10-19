37036a8187636125d9e416c31c207652367311ed
HaekyungSeong <haekyungs95@gmail.com>
2023-10-19 18:34:24 +0900

    카테고리와 포토카드 영역 구분 및 적용

--- my_flutter_app/lib/photoCard.dart
+++ my_flutter_app/lib/photoCard.dart
@@ -9,12 +9,13 @@ class PhotoCardScreen extends StatelessWidget {
         theme: ThemeData(useMaterial3: true),
         home: const Scaffold(
           body: Row(
-            children: [Toggle(), Text('안농?')],
+            children: [Toggle(), PhotoCard()],
           ),
         ));
   }
 }
 
+// 카테고리 위젯
 class Toggle extends StatelessWidget {
   const Toggle({super.key});
 
@@ -32,22 +33,78 @@ class Toggle extends StatelessWidget {
   }
 }
 
+class PhotoCard extends StatefulWidget {
+  const PhotoCard({super.key});
 
-  // @override
-  // Widget build(BuildContext content){
-  //   return Row(
-  //     children: <Widget>[
-  //       Column(
-  //         children: [
-  //       ElevatedButton(onPressed: () {}, child: const Text('2017')),
-  //       ElevatedButton(onPressed: () {}, child: const Text('2018')),
-  //       ElevatedButton(onPressed: () {}, child: const Text('2019')),
-  //       ElevatedButton(onPressed: () {}, child: const Text('2020')),
-  //       ElevatedButton(onPressed: () {}, child: const Text('2021')),
-  //       ElevatedButton(onPressed: () {}, child: const Text('2022')),
-  //       ElevatedButton(onPressed: () {}, child: const Text('2023'))
-  //     ],
-  //     )
-  //     ],
-  //   );
-  // }
+  @override
+  State<PhotoCard> createState() => _PhotoCard();
+}
+
+class _PhotoCard extends State<PhotoCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Stack(
+      children: <Widget>[
+        Positioned(
+          top: 10,
+          child: Container(
+            width: 200,
+            height: 320,
+            color: Colors.blue,
+            child: const Text('나는 상단 가운데 있을거야'),
+          ),
+        ),
+        Positioned(
+          top: 150,
+          right: 0,
+          child: Container(
+            width: 200,
+            height: 320,
+            color: Colors.red,
+            child: const Text('나는 우측에 있을거야'),
+          ),
+        ),
+        Positioned(
+          top: 300,
+          right: 0,
+          child: Container(
+            width: 200,
+            height: 320,
+            color: Colors.blue,
+            child: const Text('나는 우측 하단 있을거야'),
+          ),
+        ),
+        Positioned(
+          bottom: 10,
+          child: Container(
+            width: 200,
+            height: 320,
+            color: Colors.red,
+            child: const Text('나는 하단 가운데 있을거야'),
+          ),
+        ),
+        Positioned(
+          top: 300,
+          left: 0,
+          child: Container(
+            width: 200,
+            height: 320,
+            color: Colors.blue,
+            child: const Text('나는 좌측 하단 있을거야'),
+          ),
+        ),
+        Positioned(
+          top: 150,
+          left: 0,
+          child: Container(
+            width: 200,
+            height: 320,
+            color: Colors.red,
+            child: const Text('나는 좌측 상단 있을거야'),
+          ),
+        ),
+      ],
+    ));
+  }
+}
